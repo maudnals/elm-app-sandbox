b4340f3dbcaf7ed9d1e98852d6d7c9c48f9befca
maudnals <maud.nalpas@gmail.com>
2018-03-17 11:40:45 +0100

    added mocked backend

---
+++ api.js
@@ -0,0 +1,15 @@
+var jsonServer = require('json-server');
+
+// create an express server
+let server = jsonServer.create();
+
+// set defaults middlewares
+server.use(jsonServer.defaults());
+
+// set routing
+let router = jsonServer.router('db.json');
+server.use(router);
+
+// start server
+console.log('listening at 4000');
+server.listen(4000);
